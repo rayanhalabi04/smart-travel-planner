8d599773e99045920ad9b48596be71cfb97c3e43
rayanhalabi <rayanhalabi43@gmail.com>
2026-04-29 22:42:41 +0300

    logging

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -2,9 +2,10 @@ import logging
 import sys
 
 
-def setup_logging() -> None:
+def setup_logging(log_level: str = "INFO") -> None:
     logging.basicConfig(
-        level=logging.INFO,
+        level=getattr(logging, log_level.upper(), logging.INFO),
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         handlers=[logging.StreamHandler(sys.stdout)],
+        force=True,
     )
